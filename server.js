339c4dc08bfc8e5fd7d754c727e2eea646f9fa69
negasimichael <negasimichael1@gmail.com>
2021-03-22 21:49:17 -0500

    deploying

--- server.js
+++ server.js
@@ -15,10 +15,24 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+)
+.then(()=> console.log('mongodb connected'))
+.catch(error => console.log('mongodb connection', error));
+
+
+
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
